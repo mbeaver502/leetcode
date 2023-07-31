86b2066d2d5045ea1d3728b6afa055b4930eb60d
mbeaver502 <jmbeaver1@gmail.com>
2023-07-31 08:24:02 -0500

    Small refactor to remove unnecessary variables

--- php/add-two-numbers/src/Solution.php
+++ php/add-two-numbers/src/Solution.php
@@ -27,17 +27,15 @@ class Solution
         }
 
         $a = 0;
-        $l1Next = null;
         if ($l1 !== null) {
             $a = $l1->val;
-            $l1Next = $l1->next;
+            $l1 = $l1->next;
         }
 
         $b = 0;
-        $l2Next = null;
         if ($l2 !== null) {
             $b = $l2->val;
-            $l2Next = $l2->next;
+            $l2 = $l2->next;
         }
 
         $c = $a + $b;
@@ -46,18 +44,18 @@ class Solution
             $c %= 10;
 
             // Move the carry digit forward
-            if ($l1Next !== null) {
-                $l1Next->val += 1;
-            } else if ($l2Next !== null) {
-                $l2Next->val += 1;
+            if ($l1 !== null) {
+                $l1->val += 1;
+            } else if ($l2 !== null) {
+                $l2->val += 1;
             } else {
-                $l1Next = new ListNode(1, null);
+                $l1 = new ListNode(1, null);
             }
         }
 
         return new ListNode(
             $c,
-            $this->addTwoNumbers($l1Next, $l2Next)
+            $this->addTwoNumbers($l1, $l2)
         );
     }
 }
